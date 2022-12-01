f0b5a6dc81e4b113f3a872b88e098d404b41aa2b
Mohammad Zaid <mohammadzaid@Fathimas-MacBook-Air.local>
2022-12-01 11:01:49 +0530

    task 7 (create first component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <p>this will also be visible.. </p>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,22 @@
+function ExpenseItem() {
+  return (
+    // <div>
+    //   <div> March 28th 2022</div>
+    //   <div>
+    //     <h2>car Insurance</h2>
+    //     <div>$878.87</div>
+    //   </div>
+    // </div>
+
+    <div>
+      <div> Expense Item</div>
+      <div>
+        <h4>Food Rs 10</h4>
+        <h4>petrol Rs 100</h4>
+        <h4>Movie Rs 2000</h4>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
