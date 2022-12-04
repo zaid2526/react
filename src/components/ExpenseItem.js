084d4d46e380ccce4fb79e833b7f0902da114465
mohd zaid <mzr16894@gmail.com>
2022-12-04 12:57:33 +0530

    task 8 ( adding css styling)

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,21 +1,26 @@
-function ExpenseItem() {
-  return (
-    // <div>
-    //   <div> March 28th 2022</div>
-    //   <div>
-    //     <h2>car Insurance</h2>
-    //     <div>$878.87</div>
-    //   </div>
-    // </div>
+import './ExpenseItem.css'
 
-    <div>
-      <div> Expense Item</div>
-      <div>
-        <h4>Food Rs 10</h4>
-        <h4>petrol Rs 100</h4>
-        <h4>Movie Rs 2000</h4>
+function ExpenseItem() {
+    const expenseDate=new Date(2012,12,4)
+    const expenseTitle= "car Insurance"
+    const expensePrice= 2425
+  return (
+    <div className="expense-item">
+      <div> {expenseDate.toDateString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expensePrice}</div>
       </div>
     </div>
+
+    // <div>
+    //   <div> Expense Item</div>
+    //   <div>
+    //     <h4>Food Rs 10</h4>
+    //     <h4>petrol Rs 100</h4>
+    //     <h4>Movie Rs 2000</h4>
+    //   </div>
+    // </div>
   );
 }
 
