c7c64bc8a3fde08461c9c83999606e33ae1c66ed
mohd zaid <mzr16894@gmail.com>
2022-12-05 12:26:23 +0530

    task 12 file structuring

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
 
 import React from "react";
-function App() {
+const App=()=> {
   
 
   return (
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,7 +1,7 @@
 import './Expenses.css'
 
 import ExpenseItem from "./ExpenseItem";
-import Card from './Card'
+import Card from '../UI/Card'
 
 function Expenses(){
     const expenses = [
