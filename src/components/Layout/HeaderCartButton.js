f18c2c24be362f9dbf385100810502a311e84f2b
mzaid2526 <mohammad@vapourmeta.com>
2025-04-18 21:02:58 +0530

    Building the header of your restaurant app

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,17 @@
+import React from "react";
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css"
+
+const HeaderCartButton = (props) => {
+  return (
+    <button className={classes.button} >
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Cart</span>
+      <span className={classes.badge}>2</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
