a31fbff2b81ac875014536a99d99db74920e7692
mzaid2526 <mohammad@vapourmeta.com>
2025-04-20 09:10:33 +0530

    task-3 Making the Items visible

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Fragment } from "react";
 import Header from "./components/Layout/Header";
-import MealsSummary from "./components/Meals/MealsSummary";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <Fragment>
       <Header />
       <main>
-        <MealsSummary />
+        <Meals />
       </main>
     </Fragment>
   );
