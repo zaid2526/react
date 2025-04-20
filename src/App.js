e105b422d6b6515b19de6cc80e2adc146703ee86
mzaid2526 <mohammad@vapourmeta.com>
2025-04-20 08:17:38 +0530

    task-2 Add meals summary

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Fragment } from "react";
 import Header from "./components/Layout/Header";
+import MealsSummary from "./components/Meals/MealsSummary";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <MealsSummary />
+      </main>
     </Fragment>
   );
 }
